dda31a79c767a7b90687517a320b89a249bf61a3
JFHarris <JFHarris1983@gmail.com>
2022-07-28 14:59:17 -0500

    Docker is acting up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUser, faSearch, faKey, faSignOutAlt, faFolder,
+  faStar, faChartLine, faCalendar, faClock, faArrowRight,
+  faArrowLeft, faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faUser, faSearch, faKey, faSignOutAlt, faFolder,
+  faStar, faChartLine, faCalendar, faClock, faArrowRight, faArrowLeft, faBars, faXmark);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
